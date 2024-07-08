1776ae714146d945e57344c4c155eb57a1fef40d
Leon Matthews <leon@digitaladvisor.nz>
2024-07-08 13:58:09 +1200

    Finished! So pretty!

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -2,18 +2,31 @@
 use image::ColorType;
 use image::png::PNGEncoder;
 use num::Complex;
+use std::env;
 use std::fs::File;
 use std::str::FromStr;
 
 
 fn main() {
-    let z = Complex { re: -0.75, im: 0.1 };
-    let time = escape_time(z, 1_000);
-    if time == None {
-        println!("Did not escape!");
-    } else {
-        println!("Escaped after {:?} loops", time);
+    // Parse args
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 5 {
+        eprintln!("usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("eg. {} mandelbrot.png 1500x1000 -2.0,1.0 0.5,-1.0", args[0]);
+        std::process::exit(1);
     }
+    let bounds = parse_pair::<usize>(&args[2], 'x')
+        .expect("Error parsing image dimensions");
+    let top_left = parse_complex(&args[3])
+        .expect("Error parsing top-left corner point");
+    let bottom_right = parse_complex(&args[4])
+        .expect("Error parsing bottom-right corner point");
+
+    // Render and save image
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, top_left, bottom_right);
+    write_image(&args[1], &pixels, bounds)
+        .expect("Error writing PNG file");
 }
 
 

84176ce0cd570609af029de75604aff9c3c5ccad
Leon Matthews <leon@digitaladvisor.nz>
2024-07-08 13:30:50 +1200

    Create PNG image from pixel buffer

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,5 +1,8 @@
 
+use image::ColorType;
+use image::png::PNGEncoder;
 use num::Complex;
+use std::fs::File;
 use std::str::FromStr;
 
 
@@ -123,6 +126,17 @@ fn render(
             };
         }
     }
+}
+
+
+/// Create a greyscale PNG image from the pixel buffer.
+fn write_image(
+    filename: &str, pixels: &[u8], bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
 }
 
 

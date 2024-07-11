a4c62e1c1542ad102b65275eac1a5b5dcaa9696a
Leon Matthews <leon@digitaladvisor.nz>
2024-07-12 10:32:33 +1200

    Group scripts from Rust By Example book together

---
+++ rust_by_example/binding_and_types/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+/// Rust by Example, Chapters 4 & 5
+/// https://doc.rust-lang.org/rust-by-example/variable_bindings.html
+/// https://doc.rust-lang.org/rust-by-example/types.html
+
+fn main() {
+    println!("Hello, world!");
+}

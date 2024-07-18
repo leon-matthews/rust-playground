524d9a48a0b0170cae464ca384f6467e2613e0ea
Leon Matthews <leon@digitaladvisor.nz>
2024-07-18 15:55:17 +1200

    Not much example code to work through...

--- oreilly_rust/error_handling/src/main.rs
+++ oreilly_rust/error_handling/src/main.rs
@@ -1,3 +1,10 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+/// Programming Rust (O'Reilly, 2nd Edition)
+/// Chapter 7: Error Handling
+
+/// Error handling is all about `panic` or `Result`
 fn main() {
-    println!("Hello, world!");
+    panic!("Hello, world!");
 }

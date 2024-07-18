e58547f1a34bc0ee62a897991b0f77654f3d444b
Leon Matthews <leon@digitaladvisor.nz>
2024-07-18 13:11:33 +1200

    Finish chapter 6, on to chapter 7

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rust-playground
 
+Snippets, experiments, and projects that haven't grown legs yet.
--- oreilly_rust/expressions/src/main.rs
+++ oreilly_rust/expressions/src/main.rs
@@ -6,5 +6,17 @@
 
 
 fn main() {
-    println!("Hello, world!");
+    // Most of this chapter is straight-forward, but closure's syntax is new
+    closures();
+}
+
+
+fn closures() {
+    let is_even = |x| x % 2 == 0;
+
+    // Block required if return type specified explicitly
+    let is_even_explicit = |x: u64| -> bool { x % 2 == 0 };
+
+    println!("{}", is_even(4));
+    println!("{}", is_even_explicit(4545443));
 }

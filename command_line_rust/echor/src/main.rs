cee5f714054d2aa5974f4a80f50033c5c6f462d9
Leon Matthews <leon@digitaladvisor.nz>
2024-07-19 12:34:09 +1200

    First implementation seems to work

--- command_line_rust/echor/src/main.rs
+++ command_line_rust/echor/src/main.rs
@@ -1,17 +1,18 @@
-
 /// Command-Line Rust
 /// Chapter 2
 ///
 /// Process command-line arguments, deal with errors, stack, and heap memory.
 
+use std::io::{self, Write};
 
 use clap::Parser;
+
 
 #[derive(Debug, Parser)]
 #[command(about, version)]
 struct Args {
     /// Do not output the trailing newline
-    #[arg(short='n')]
+    #[arg(short = 'n')]
     omit_newline: bool,
 
     /// Echo text to standard output
@@ -21,6 +22,7 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
-    dbg!(&args);
+    let ending = if args.omit_newline { "" } else { "\n" };
+    let buffer = format!("{}{}", args.text.join(" "), ending).into_bytes();
+    let _ = io::stdout().write_all(&buffer);
 }

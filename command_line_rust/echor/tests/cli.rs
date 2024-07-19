fe9fec09e5d56b4bf7aafcd3ff02c93ac40fcdfd
Leon Matthews <leon@digitaladvisor.nz>
2024-07-19 13:50:21 +1200

    Finish the chapter with lots of new tests

--- command_line_rust/echor/tests/cli.rs
+++ command_line_rust/echor/tests/cli.rs
@@ -4,30 +4,50 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+/// Test our command against files of expected output
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
 
 /// Running with zero arguments should fail, printing usage string to stderr
 /// (Actual GNU coreutils echo doesn't do this, but that's pedagogy for you!)
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage:"));
+    Ok(())
 }
 
-
-/// Most basic example
+/// Compare with `echo "Hello there"`
 #[test]
-fn run() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello").assert().success().stdout("Hello\n");
+fn hello1() -> TestResult {
+    run(&["Hello there"], &"tests/expected/hello1.txt")
 }
 
-
-/// Compare with output from `echo "Hello there"`
+/// Compare with `echo -n "Hello there"`
 #[test]
-fn hello1() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    let expected = fs::read_to_string("tests/expected/hello1.txt").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1_no_newline() -> TestResult {
+    run(&["-n", "Hello there"], &"tests/expected/hello1.n.txt")
+}
+
+/// Compare against 'echo "Hello" "There"'
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], &"tests/expected/hello2.txt")
+}
+
+/// Compare against 'echo -n "Hello" "There"'
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], &"tests/expected/hello2.n.txt")
 }

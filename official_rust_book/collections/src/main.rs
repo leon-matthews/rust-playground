49d18c20ce1cccf6233a17ced090c2dcf7b49794
Leon Matthews <leon@lost.co.nz>
2024-07-20 23:11:12 +1200

    Start working through String example

--- official_rust_book/collections/src/main.rs
+++ official_rust_book/collections/src/main.rs
@@ -4,6 +4,44 @@
 /// Chapter 8 - Common Collections
 /// https://doc.rust-lang.org/book/ch08-00-common-collections.html
 
+
+fn strings() {
+    // Create empty, append &str
+    let mut s1 = String::new();
+    s1.push_str("This one was empty!");
+    println!("{s1}");
+
+    // Create from &str, and char
+    let mut s2 = "This was a &str type".to_string();
+    s2.push('!');
+    println!("{s2}");
+
+    // Concatanate &str literals and strings, consuming first string.
+    let s3 = s1 + " AND " + &s2;
+    println!("{}", s3);
+
+    // TODO: Continue from listing 8.18
+
+    /// ALL strings are UTF-8
+    let hellos = vec![
+        String::from("السلام عليكم"),
+        String::from("Dobrý den"),
+        String::from("Hello"),
+        String::from("שלום"),
+        String::from("नमस्ते"),
+        String::from("こんにちは"),
+        String::from("안녕하세요"),
+        String::from("你好"),
+        String::from("Olá"),
+        String::from("Здравствуйте"),
+        String::from("Hola"),
+    ];
+    for hello in &hellos {
+        println!("Hello? {hello}!");
+    }
+}
+
+
 fn vectors() {
     // Let's create a vector containing a non-copyable types.
     let mut v = Vec::new();
@@ -54,4 +92,5 @@ fn vectors() {
 
 fn main() {
     vectors();
+    strings();
 }

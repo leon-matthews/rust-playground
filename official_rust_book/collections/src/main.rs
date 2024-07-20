23ad215d6ef593cc2bc76861e8f202f214f53251
Leon Matthews <leon@lost.co.nz>
2024-07-20 20:07:43 +1200

    Start chapter on common collection types

---
+++ official_rust_book/collections/src/main.rs
@@ -0,0 +1,9 @@
+
+/// Official Rust Book
+///
+/// Chapter 8 - Common Collections
+/// https://doc.rust-lang.org/book/ch08-00-common-collections.html
+
+fn main() {
+    println!("Hello, world!");
+}

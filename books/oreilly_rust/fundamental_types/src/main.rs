d21119bb0ecc57c28d19736b33660bca1a25f4e0
Leon Matthews <leon@digitaladvisor.nz>
2024-07-26 12:42:47 +1200

    Fundamentals: slices, and strings

--- books/oreilly_rust/fundamental_types/src/main.rs
+++ books/oreilly_rust/fundamental_types/src/main.rs
@@ -1,13 +1,18 @@
 
 fn main() {
-    integer_overflow();
-    let primes = sieve_of_eratosthenes();
-    println!("There are {} primes less than 1,000,000", primes.len());
-    println!("Vector has room for {}", primes.capacity());
+    integers();
     bools();
     characters();
     tuples();
     pointers();
+
+    // Arrays and Vectors
+    let primes = sieve_of_eratosthenes();
+    println!("There are {} primes less than 1,000,000", primes.len());
+    println!("Vector has room for {}", primes.capacity());
+
+    slices();
+    strings();
     closures_are_variables();
     functions_are_variables(hello);
 }
@@ -21,6 +26,7 @@ fn pointers() {
     // `&T`     Immutable, shared reference. Read-only.
     // `&mut T` Mutable, exclusive reference.
 
+
     // Boxes
     /////////
     // Force allocation on the heap
@@ -28,15 +34,23 @@ fn pointers() {
     let b = Box::new(t);        // Type `Box<(i32, &str)>`
 
     // Compare sizes on stack
-    // On 64-bit Linux the actual values are 24 bytes and 8 bytes.
-    // 8 bytes = Single pointer
-    // 24 bytes = 8 for integer (4 plus padding), 8 for str pointer, 8 for length
+    // 64-bit Linux the actual values are 24 bytes and 8 bytes:
+    //   8 bytes = One 64-bit pointer
+    //   24 bytes = 8 for integer (4 plus padding), 8 for str pointer, 8 for length
+    // 32-bit Linux the values are 12 and 4 bytes
+    //   4 bytes = One 32-bit poniter
+    //  12 bytes = 4 for integer, 4 for str pointer, and another 4 for its length.
     println!("Size of egg tuple is {} bytes", size_of_val(&t));
     println!("Size of `Box` of eggs is {} bytes", size_of_val(&b));
 
     // Automatic dereferences using dot operator
     println!("I have {} {}", (*b).0, (*b).1);
     println!("I have {} {}", b.0, b.1);
+
+    // Raw Pointers
+    ////////////////
+    // Really just C/C++ pointers, with same type, eg. `*mut T` and `*const T`
+    // Only usable in an `unsafe` block.
 }
 
 
@@ -112,7 +126,7 @@ fn hello(who: &str) -> bool {
 
 
 /// Overflow behaviour is defined, and configurable
-fn integer_overflow() {
+fn integers() {
     // Overflow will panic by default - but only in debug builds
     let mut i: u128 = 1;
 
@@ -139,6 +153,7 @@ fn functions_are_variables(greeting: fn(&str) -> bool) {
 
 /// Use an array of bools to calculate primes
 fn sieve_of_eratosthenes() -> Vec<u32> {
+    // Big truthy array!
     let mut sieve = [true; 1_000_000];
 
     // Zero and one are not primes by definition
@@ -176,4 +191,96 @@ fn sieve_of_eratosthenes() -> Vec<u32> {
     }
 
     primes
+}
+
+
+/// Named region of array or vector
+fn slices() {
+    // Array vs vector
+    let array: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
+    let vector: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+
+    // Rust will convert the RHS references to the slice type we specified
+    let array_slice: &[f64] = &array;
+    let vector_slice: &[f64] = &vector;
+
+    /// Function can print contents of both our array and our vector!
+    fn print_floats(numbers: &[f64]) {
+        for n in numbers {
+            print!("{n} ")
+        }
+        println!();
+    }
+
+    print_floats(array_slice);
+    print_floats(vector_slice);
+
+    // Slicing out part by indexing by range is more usual
+    print_floats(&array[1..3]);
+    print_floats(&vector[3..]);
+}
+
+
+/// We have strings and string slices
+fn strings() {
+    // Same backslash escape sequences as char literals
+    let _speech = "\"Ouch!\" said the well.\n";
+
+    // Multiline literals include newline and following indentation
+    println!("In the room the women come and go,
+        Singing of Mount Abora");
+
+    // Ending line with backslash suppresses that whitespace
+    println!("In the room the women come and go, \
+        Singing of Mount Abora");
+
+    // Raw strings ignore backslash sequences
+    let _pattern = r"\d+";
+
+    // POUND IT RAW!
+    println!(r###"
+        This string literal will not end until a " followed by ###
+        You can use as many or as few hash characters as you like.
+    "###);
+
+    // Byte string literals are &[u8] - a slice of bytes, not unicode text.
+    let method = b"GET";
+    assert_eq!(method, &[71, 69, 84]);
+
+    // C-String literal `&CStr`
+    let old_fashioned = c"Null Terminated";
+    println!("{:?}", old_fashioned);
+
+    // The len() method returns length of utf-8 string in bytes, not chars
+    assert_eq!("ಠ_ಠ".len(), 7);
+    assert_eq!("ಠ_ಠ".chars().count(), 3);       // Create then consume iterator
+
+    // Building strings...
+    // ...from `&'static str` literals
+    let _s1 = "Copy and convert &str to String".to_string();
+    let _s2 = "Same as above".to_owned();
+    let _s3 = format!("{}°{:02}'{:02}\"N", 24, 5, 6);
+    let _s4 = String::from("Another string here!");
+    println!("{}", _s3);
+
+    // From iterable
+    let parts = vec!["veni", "vidi", "vici"];
+    assert_eq!(parts.concat(), "venividivici");
+    assert_eq!(parts.join(", "), "veni, vidi, vici");
+
+    // Methods are usually found on &str
+    assert!("ONE".to_lowercase() == "one");
+    assert!("peanut".contains("nut"));
+    assert_eq!("ಠ_ಠ".replace("ಠ", "█"), "█_█");
+    for word in "veni, vidi, vici".split(", ") {
+        assert!(word.starts_with("v"));
+    }
+
+    /*
+    Non UTF-8 strings:
+        - For filenames use `std::path::PathBuf` and `&Path`
+        - Binary strings use `Vec<u8>` and `&[u8]`
+        - Command-line args and environment variables use `OsString` and `&OsStr`
+        - Interacting with C libraries use `std::ffi::CString` and `&CStr`
+    */
 }

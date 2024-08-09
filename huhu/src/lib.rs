fe8d9d9a4792966708ee131a534a0872d0960ea2
Leon Matthews <leon@digitaladvisor.nz>
2024-08-09 20:39:21 +1200

    Open file or standard input

---
+++ huhu/src/lib.rs
@@ -0,0 +1,14 @@
+
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+use anyhow::Result;
+
+
+/// Open file or stdin
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}

c1ec1ca098ad6c6706325d05f4f90f484fe071e4
Leon Matthews <leon@digitaladvisor.nz>
2024-08-12 11:35:35 +1200

    Experiment with collecting results

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,19 +1,7 @@
 
-use log::{error, warn, info, debug, trace};
-
-
 fn main() {
-    // Quick start: use default initialization
-    colog::init();
-
-    error!("error message");
-    error!("error with fmt: {}", 42);
-    warn!("warn message");
-    info!("info message");
-    debug!("debug message"); // not printed (LogLevel::Info is the default level)
-    trace!("trace message"); // not printed (LogLevel::Info is the default level)
-
-    // notice how multi-line comments are handled gracefully
-    info!("multi line demonstration\nhere");
-    info!("more\nmulti\nline\nhere\nhere");
+    // Collect into a result
+    let results = [Ok(1), Err("nope"), Ok(3), Err("bad")];
+    let result: Vec<_> = results.iter().map(|n| n).collect();
+    dbg!(&result);
 }

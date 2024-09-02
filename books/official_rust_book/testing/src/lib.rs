84100f225e8a0a0bddad308691e87a570e7097e3
Leon Matthews <leon@digitaladvisor.nz>
2024-09-02 20:18:19 +1200

    Try moving tests into their own module

--- books/official_rust_book/testing/src/lib.rs
+++ books/official_rust_book/testing/src/lib.rs
@@ -36,47 +36,4 @@ impl Rectangle {
 
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Leon");
-        assert!(
-            result.contains("Leon"),
-            "Greeting does not contain name: {result:?}"
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn greater_than_100() {
-        Guess::new(200);
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_can_not_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-        assert!(!smaller.can_hold(&larger));
-    }
-}
+mod tests;

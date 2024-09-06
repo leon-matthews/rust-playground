4e9301958c6f29b4c2b9ebaf1c0b92a91405a7f6
Leon Matthews <leon@digitaladvisor.nz>
2024-09-07 10:01:25 +1200

    Experiment with return values

--- morse_trainer/src/lib.rs
+++ morse_trainer/src/lib.rs
@@ -1,15 +1,16 @@
+#![allow(dead_code)]
 
 /**
 The various possibles things that a Morse transmission can contain.
 */
-enum Morse {
+#[derive(Debug)]
+pub enum Morse {
     Dit,            // 1 time unit on
     Dah,            // 3 dits on
     InterGap,       // 1 dit off between elements
     ShortGap,       // 3 dits off between letters
     MediumGap,      // 7 dits off between words
 }
-
 
 /**
 Return true if given byte is an ascii character transmitable in Morse Code.
--- morse_trainer/src/main.rs
+++ morse_trainer/src/main.rs
@@ -1,3 +1,23 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use morse_trainer::Morse::{self, *};
+
+
 fn main() {
-    println!("Hello, world!");
+    let bytes = vec![b's', b't', b'e', b'l', b'l', b'a'];
+    let codes: Vec<Morse> = bytes.iter()
+        .map(|byte| match byte {
+            b'a' => [Dit, Dah],
+            b'b' => [Dah, Dit, Dit, Dit],
+            b'c' => [Dah, Dit, Dah, Dit],
+            b'd' => [Dah, Dit, Dit],
+            b'e' => [Dit],
+            b'f' => [Dit, Dit, Dah, Dit],
+            _ => [],
+        })
+        .flatten()
+        .collect();
+    println!("{codes:?}");
 }

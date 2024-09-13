9602d881e386edb004ece553a007d9055af49f26
Leon Matthews <leon@digitaladvisor.nz>
2024-09-14 09:48:05 +1200

    Experiment with implicit vs explicit usage of Default::default()

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,5 +1,35 @@
+#![allow(dead_code)]
+
+
+#[derive(Debug, Default)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+    alpha: u8,
+}
+
 
 fn main() {
-    let b = b"stella";
-    println!("{b:?}");
+    // Full default
+    let c = Color::default();
+    println!("{:?}", c);
+
+    // Implicit default
+    let c: Color = Default::default();
+    println!("{:?}", c);
+
+    // Default using struct update syntax
+    let c2 = Color {
+        blue: 255,
+        ..Color::default()
+    };
+    println!("{:?}", c2);
+
+    // Implicit using struct update syntax
+    let c3 = Color {
+        red: 255,
+        ..Default::default()
+    };
+    println!("{:?}", c3);
 }

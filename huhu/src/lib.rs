98cfd5dd7fa100add32fd00dd2c4ed3aa8c57b5a
Leon Matthews <leon@digitaladvisor.nz>
2024-09-16 10:10:13 +1200

    Open file and count its lines

--- huhu/src/lib.rs
+++ huhu/src/lib.rs
@@ -6,8 +6,17 @@ use std::io::{self, BufRead, BufReader};
 use anyhow::Result;
 
 
+pub fn count_lines(file: impl BufRead) -> Result<usize> {
+    let mut count: usize = 0;
+    for _ in file.lines() {
+        count += 1;
+    }
+    Ok(count)
+}
+
+
 /// Open file or stdin
-fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+pub fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
--- huhu/src/main.rs
+++ huhu/src/main.rs
@@ -1,18 +1,22 @@
 #![allow(unused_variables)]
 
+use anyhow::Result;
 use clap::Parser;
+
+use huhu::{count_lines, open};
 
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 struct Args {
-    /// Input file(s)
-    #[arg(default_value="-")]
-    files: Vec<String>,
+    path: std::path::PathBuf,
 }
 
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    println!("{args:?}");
+    let file = open(args.path.to_str().unwrap()).unwrap();
+    let count = count_lines(file).unwrap();
+    println!("There were {count} lines in given log file");
+    Ok(())
 }

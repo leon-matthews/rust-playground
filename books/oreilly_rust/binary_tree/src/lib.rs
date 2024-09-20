2b3ef89229bff1475cca626d3c2ca218ce89835d
Leon Matthews <leon@lost.co.nz>
2024-09-21 11:41:18 +1200

    Implement add() method

--- books/oreilly_rust/binary_tree/src/lib.rs
+++ books/oreilly_rust/binary_tree/src/lib.rs
@@ -1,16 +1,38 @@
 
 #[derive(Debug)]
-enum BinaryTree<T> {
+pub enum BinaryTree<T> {
     Empty,
     NonEmpty(Box<TreeNode<T>>),
 }
 
 
 #[derive(Debug)]
-struct TreeNode<T> {
+pub struct TreeNode<T> {
     element: T,
     left: BinaryTree<T>,
     right: BinaryTree<T>,
+}
+
+
+impl<T: Ord> BinaryTree<T> {
+    pub fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }))
+            },
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            },
+        }
+    }
 }
 
 
@@ -20,8 +42,35 @@ mod tests {
     use indoc::indoc;
     use pretty_assertions::assert_eq;
 
+    const SINGLE_NODE_EXPECTED: &str = concat!(
+        "NonEmpty(TreeNode { element: \"Jupiter\", ",
+        "left: Empty, right: Empty })"
+    );
+
+    const SMALL_TREE_EXPECTED: &str = indoc! {r#"
+        NonEmpty(
+            TreeNode {
+                element: "Mars",
+                left: NonEmpty(
+                    TreeNode {
+                        element: "Jupiter",
+                        left: Empty,
+                        right: Empty,
+                    },
+                ),
+                right: NonEmpty(
+                    TreeNode {
+                        element: "Mercury",
+                        left: Empty,
+                        right: Empty,
+                    },
+                ),
+            },
+        )"#
+    };
+
     #[test]
-    fn test_single_node() {
+    fn test_single_node_manually() {
         let tree = BinaryTree::NonEmpty(
             Box::new(TreeNode {
                 element: "Jupiter",
@@ -30,14 +79,27 @@ mod tests {
             })
         );
 
-        assert_eq!(
-            format!("{tree:?}"),
-            "NonEmpty(TreeNode { element: \"Jupiter\", left: Empty, right: Empty })",
-        );
+        assert_eq!(format!("{tree:?}"), SINGLE_NODE_EXPECTED);
     }
 
     #[test]
-    fn test_small_tree() {
+    fn test_single_node_add() {
+        let mut tree = BinaryTree::Empty;
+        tree.add("Jupiter");
+        assert_eq!(format!("{tree:?}"), SINGLE_NODE_EXPECTED);
+    }
+
+    #[test]
+    fn test_small_tree_add() {
+        let mut tree = BinaryTree::Empty;
+        tree.add("Mars");
+        tree.add("Jupiter");
+        tree.add("Mercury");
+        assert_eq!(format!("{tree:#?}"), SMALL_TREE_EXPECTED);
+    }
+
+    #[test]
+    fn test_small_tree_manually() {
         let jupiter = BinaryTree::NonEmpty(
             Box::new(TreeNode {
                 element: "Jupiter",
@@ -62,29 +124,6 @@ mod tests {
                 right: mercury,
             })
         );
-
-        println!("{mars:#?}");
-        let expected = indoc! {r#"
-            NonEmpty(
-                TreeNode {
-                    element: "Mars",
-                    left: NonEmpty(
-                        TreeNode {
-                            element: "Jupiter",
-                            left: Empty,
-                            right: Empty,
-                        },
-                    ),
-                    right: NonEmpty(
-                        TreeNode {
-                            element: "Mercury",
-                            left: Empty,
-                            right: Empty,
-                        },
-                    ),
-                },
-            )"#
-        };
-        assert_eq!(format!("{mars:#?}"), expected);
+        assert_eq!(format!("{mars:#?}"), SMALL_TREE_EXPECTED);
     }
 }
--- books/oreilly_rust/binary_tree/src/main.rs
+++ books/oreilly_rust/binary_tree/src/main.rs
@@ -1,3 +1,21 @@
+
+use binary_tree::BinaryTree;
+
 fn main() {
-    println!("Hello, world!");
+    let planets = vec![
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Neptune",
+    ];
+    println!("{planets:?}");
+
+    let mut tree = BinaryTree::Empty;
+    for planet in planets {
+        tree.add(planet);
+    }
+    println!("{tree:#?}");
 }
